226a41bd9c387cb840eb0c61f7b8b04ad107cfaf
franciscojdamico@gmail.com <37968393+damicofj@users.noreply.github.com>
2021-09-01 13:07:24 -0300

    sixth commit

--- scripts/script1.js
+++ scripts/script1.js
@@ -4,3 +4,19 @@ function openForm() {
 function closeForm() {
   document.getElementById("popupForm").style.display = "none";
 }
+
+// articles destination elements
+
+var request = new XMLHttpRequest();
+request.open("GET", "scripts/articles.json", false);
+request.send(null);
+var articles_JSON_object = JSON.parse(request.responseText);
+
+for (var i = 0; i < articles_JSON_object.destinations.length; i++) {
+    console.log(articles_JSON_object.destinations[i].destinationName);
+    console.log(articles_JSON_object.destinations[i].destinationImg);
+    console.log(articles_JSON_object.destinations[i].destinationData);
+}
+
+
+
