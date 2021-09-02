ab9233b39ff6626f9286d99093b51a5679a3d735
Francisco <franciscodamico@Franciscos-MacBook-Air.local>
2021-09-02 09:18:54 -0300

    thirteenth commit

--- scripts/script1.js
+++ scripts/script1.js
@@ -18,6 +18,8 @@ request.open("GET", "scripts/articles.json", false);
 request.send(null);
 var articles_JSON_object = JSON.parse(request.responseText);
 
+
+
 function articlesCreator(i) {
   // create each div for the destination with class "row articles"
   var divmain = document.createElement('div');
@@ -60,8 +62,50 @@ function articlesCreator(i) {
 // the others are just available to the people that decide to subscribe ?)
 
 // o lo mas logico seria crearlo sin la function creadora, es decir, a mano
-articlesCreator(0);
+// primera 
+function firstArticleCreator() {
+  // create each div for the destination with class "row articles"
+  var divmain = document.createElement('div');
+  divmain.className = "row articles-row 0";
+  // inside it create div with class "col-lg-3 articles-img"
+  var divimg = document.createElement('div');
+  divimg.className = "col-lg-3 articles-img";
+  // inside it create insert image with src from JSON
+  var imgmain = document.createElement('img');
+  imgmain.src = articles_JSON_object.destinations[0].destinationImg;
+  divimg.appendChild(imgmain);
+  divmain.appendChild(divimg);
+  
+  // inside it create div with class "col-lg-8 articles-text"
+  var divtext = document.createElement('div');
+  divtext.className = "col-lg-8 articles-text";
+  // inside it create create and insert h3 and p and button with src from JSON
+  var h3main = document.createElement('h3');
+  h3main.innerText = articles_JSON_object.destinations[0].destinationName;
+  var pmain = document.createElement('p');
+  pmain.innerText = articles_JSON_object.destinations[0].destinationData;
+  var btnmain = document.createElement('button');
+  btnmain.className = "btn btn-danger";
+  btnmain.innerText = "Read More"; 
+  
+  divtext.appendChild(h3main);
+  divtext.appendChild(pmain);
+  divtext.appendChild(btnmain);
+  
+  // append the texts to the created div
+  divmain.appendChild(divtext);
 
+  // appends each div created to our articles-div
+  var art = document.getElementById("articles-div");
+
+  art.appendChild(divmain);
+}
+
+firstArticleCreator();
+
+// el resto
 for (var i = 1; i < articles_JSON_object.destinations.length; i++) {
   articlesCreator(i);
 }
+
+// lo podia poner como src del button sino, que iba a ser mucho mas sencillo
