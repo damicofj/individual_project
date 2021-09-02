fa7186863ffc2d7be356dd7d43968bb194788f64
Francisco <franciscodamico@Franciscos-MacBook-Air.local>
2021-09-02 09:24:55 -0300

    sixteenth

--- scripts/script1.js
+++ scripts/script1.js
@@ -63,43 +63,6 @@ function articlesCreator(i) {
 
 // o lo mas logico seria crearlo sin la function creadora, es decir, a mano
 // primera 
-function firstArticleCreator() {
-  // create each div for the destination with class "row articles"
-  var divmain = document.createElement('div');
-  divmain.className = "row articles-row 0";
-  // inside it create div with class "col-lg-3 articles-img"
-  var divimg = document.createElement('div');
-  divimg.className = "col-lg-3 articles-img";
-  // inside it create insert image with src from JSON
-  var imgmain = document.createElement('img');
-  imgmain.src = articles_JSON_object.destinations[0].destinationImg;
-  divimg.appendChild(imgmain);
-  divmain.appendChild(divimg);
-  
-  // inside it create div with class "col-lg-8 articles-text"
-  var divtext = document.createElement('div');
-  divtext.className = "col-lg-8 articles-text";
-  // inside it create create and insert h3 and p and button with src from JSON
-  var h3main = document.createElement('h3');
-  h3main.innerText = articles_JSON_object.destinations[0].destinationName;
-  var pmain = document.createElement('p');
-  pmain.innerText = articles_JSON_object.destinations[0].destinationData;
-  var btnmain = document.createElement('button');
-  btnmain.className = "btn btn-danger";
-  btnmain.innerText = "Read More"; 
-  
-  divtext.appendChild(h3main);
-  divtext.appendChild(pmain);
-  divtext.appendChild(btnmain);
-  
-  // append the texts to the created div
-  divmain.appendChild(divtext);
-
-  // appends each div created to our articles-div
-  var art = document.getElementById("articles-div");
-
-  art.appendChild(divmain);
-}
 
 firstArticleCreator();
 
