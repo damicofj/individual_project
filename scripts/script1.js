ea10ed3b9f0507c55322ef35b393ecee9b1a3c02
Francisco <franciscodamico@Franciscos-MacBook-Air.local>
2021-09-08 23:19:49 -0300

    21st

--- scripts/script1.js
+++ scripts/script1.js
@@ -18,38 +18,39 @@ request.open("GET", "scripts/articles.json", false);
 request.send(null);
 var articles_JSON_object = JSON.parse(request.responseText);
 
-
-
 function articlesCreator(i) {
   // create each div for the destination with class "row articles"
-  var divmain = document.createElement('div');
+  var divmain = document.createElement("div");
   divmain.className = "row articles-row " + i;
   // inside it create div with class "col-lg-3 articles-img"
-  var divimg = document.createElement('div');
+  var divimg = document.createElement("div");
   divimg.className = "col-lg-4 articles-img";
   // inside it create insert image with src from JSON
-  var imgmain = document.createElement('img');
+  var imgmain = document.createElement("img");
   imgmain.src = articles_JSON_object.destinations[i].destinationImg;
   divimg.appendChild(imgmain);
   divmain.appendChild(divimg);
-  
+
   // inside it create div with class "col-lg-8 articles-text"
-  var divtext = document.createElement('div');
+  var divtext = document.createElement("div");
   divtext.className = "col-lg-8 articles-text";
   // inside it create create and insert h3 and p and button with src from JSON
-  var h3main = document.createElement('h3');
+  var h3main = document.createElement("h3");
   h3main.innerText = articles_JSON_object.destinations[i].destinationName;
-  var pmain = document.createElement('p');
+  var pmain = document.createElement("p");
   pmain.innerText = articles_JSON_object.destinations[i].destinationData;
-  var btnmain = document.createElement('button');
+  var btnmain = document.createElement("button");
   btnmain.className = "btn btn-danger articles-text-btn";
-  btnmain.innerText = "READ MORE"; 
-  btnmain.setAttribute("onclick", articles_JSON_object.destinations[i].destinationBtn);
-  
+  btnmain.innerText = "READ MORE";
+  btnmain.setAttribute(
+    "onclick",
+    articles_JSON_object.destinations[i].destinationBtn
+  );
+
   divtext.appendChild(h3main);
   divtext.appendChild(pmain);
   divtext.appendChild(btnmain);
-  
+
   // append the texts to the created div
   divmain.appendChild(divtext);
 
