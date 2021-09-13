b249714aee6ac78a37294136496c608447eedb88
franciscojdamico@gmail.com <37968393+damicofj@users.noreply.github.com>
2021-09-13 00:00:51 -0300

    24th

---
+++ scripts/script_articles.js
@@ -0,0 +1,64 @@
+// articles destination elements
+// to open the JSON file and get the data
+var request = new XMLHttpRequest();
+request.open("GET", "scripts/articles.json", false);
+request.send(null);
+var articles_JSON_object = JSON.parse(request.responseText);
+
+// HTML DOM manipulator
+var request = new XMLHttpRequest();
+request.open("GET", "scripts/articles.json", false);
+request.send(null);
+var articles_JSON_object = JSON.parse(request.responseText);
+
+function articlesCreator(i) {
+  // create each div for the destination with class "row articles"
+  var divmain = document.createElement("div");
+  divmain.className = "row articles-row " + i;
+  // inside it create div with class "col-lg-3 articles-img"
+  var divimg = document.createElement("div");
+  divimg.className = "col-lg-4 articles-img";
+  // inside it create insert image with src from JSON
+  var imgmain = document.createElement("img");
+  imgmain.src = articles_JSON_object.destinations[i].destinationImg;
+  divimg.appendChild(imgmain);
+  divmain.appendChild(divimg);
+
+  // inside it create div with class "col-lg-8 articles-text"
+  var divtext = document.createElement("div");
+  divtext.className = "col-lg-8 articles-text";
+  // inside it create create and insert h3 and p and button with src from JSON
+  var h3main = document.createElement("h3");
+  h3main.innerText = articles_JSON_object.destinations[i].destinationName;
+  var pmain = document.createElement("p");
+  pmain.innerText = articles_JSON_object.destinations[i].destinationData;
+  var btnmain = document.createElement("button");
+  btnmain.className = "btn btn-danger articles-text-btn";
+  btnmain.innerText = "READ MORE";
+  btnmain.setAttribute(
+    "onclick",
+    articles_JSON_object.destinations[i].destinationBtn
+  );
+
+  // appends the final results to divtext
+  divtext.appendChild(h3main);
+  divtext.appendChild(pmain);
+  divtext.appendChild(btnmain);
+
+  // append the texts to the created div
+  divmain.appendChild(divtext);
+
+  // appends each div created to our articles-div
+  var art = document.getElementById("articles-div");
+
+  // appends the final result to the DOM of the page (to the div precisely)
+  art.appendChild(divmain);
+}
+
+// thid piece of code uses the articlesCreator function
+// and appends each article remaining from articles.json 
+// to the articles page
+// after Milan 
+for (var i = 0; i < articles_JSON_object.destinations.length; i++) {
+  articlesCreator(i);
+}
